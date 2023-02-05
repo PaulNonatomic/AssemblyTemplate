1af3644bb032fdbb851688eeea2ebd3f5c7dcf78
Paul <paul@nonatomic.co.uk>
2023-02-05 23:46:32 +0000

    Added the AssemblyTemplateWindow along withthe first template

---
+++ AssemblyTemplate/Assets/com.nonatomic.assembly-template/Editor/Source/AssemblyDefinitionTemplate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Unity.Plastic.Newtonsoft.Json;
+
+namespace AssemblyTemplate.Editor
+{
+	[Serializable]
+	public class AssemblyDefinitionTemplate
+	{
+		[JsonProperty("name")]
+		public string Name;
+			
+		[JsonProperty("rootNamespace")]
+		public string RootNameSpace;
+			
+		[JsonProperty("references")]
+		public string[] References;
+			
+		[JsonProperty("includePlatforms")]
+		public string[] IncludePlatforms;
+			
+		[JsonProperty("excludePlatforms")]
+		public string[] ExcludePlatforms;
+			
+		[JsonProperty("allowUnsafeCode")]
+		public bool AllowUnsafeCode;
+			
+		[JsonProperty("overrideReferences")]
+		public bool OverrideReferences;
+			
+		[JsonProperty("precompiledReferences")]
+		public string[] PrecompiledReferences;
+			
+		[JsonProperty("autoReferenced")]
+		public bool AutoReferenced = true;
+			
+		[JsonProperty("defineConstraints")]
+		public string[] DefineConstraints;
+			
+		[JsonProperty("versionDefines")]
+		public string[] VersionDefines;
+			
+		[JsonProperty("noEngineReferences")]
+		public bool NoEngineReferences;
+	}
+}
